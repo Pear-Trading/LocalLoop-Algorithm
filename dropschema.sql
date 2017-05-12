b5a7e1c46a6cbfacaeb74858fc86ced00a6c8093
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-12 01:53:56 +0100

    Updated drop sql statements to only drop them if they exist.

--- dropschema.sql
+++ dropschema.sql
@@ -1,23 +1,23 @@
-DROP VIEW AllLoops_ViewActive;
-DROP INDEX AllLoops_IndexTransactionId;
-DROP TABLE AllLoops;
+DROP VIEW IF EXISTS AllLoops_ViewActive;
+DROP INDEX IF EXISTS AllLoops_IndexTransactionId;
+DROP TABLE IF EXISTS AllLoops;
 
-DROP VIEW AllLoopsStats_ViewActive;
-DROP INDEX AllLoopsStats_IndexActive;
-DROP TABLE AllLoopsStats;
+DROP VIEW IF EXISTS AllLoopsStats_ViewActive;
+DROP INDEX IF EXISTS AllLoopsStats_IndexActive;
+DROP TABLE IF EXISTS AllLoopsStats;
 
-DROP TABLE CandinateTransaction;
+DROP TABLE IF EXISTS CandinateTransaction;
 
-DROP TABLE CurrentChains;
+DROP TABLE IF EXISTS CurrentChains;
 
-DROP TABLE LastUserTransaction;
+DROP TABLE IF EXISTS LastUserTransaction;
 
-DROP VIEW ProcessedTransactions_ViewIncludedHerusticDesc;
-DROP VIEW ProcessedTransactions_ViewIncludedHerusticAsc;
-DROP VIEW ProcessedTransactions_ViewIncluded;
-DROP INDEX ProcessedTransactions_IndexValue;
-DROP INDEX ProcessedTransactions_IndexToUserId;
-DROP INDEX ProcessedTransactions_IndexFromUserId;
+DROP VIEW IF EXISTS ProcessedTransactions_ViewIncludedHerusticDesc;
+DROP VIEW IF EXISTS ProcessedTransactions_ViewIncludedHerusticAsc;
+DROP VIEW IF EXISTS ProcessedTransactions_ViewIncluded;
+DROP INDEX IF EXISTS ProcessedTransactions_IndexValue;
+DROP INDEX IF EXISTS ProcessedTransactions_IndexToUserId;
+DROP INDEX IF EXISTS ProcessedTransactions_IndexFromUserId;
 
-DROP TABLE ProcessedTransactions;
-DROP TABLE OriginalTransactions;
+DROP TABLE IF EXISTS ProcessedTransactions;
+DROP TABLE IF EXISTS OriginalTransactions;
