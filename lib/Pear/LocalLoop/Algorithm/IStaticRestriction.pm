a34dff840ddbc91d03579c435be2be382455bdcc
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-12 02:43:47 +0100

    Add comment onto the IStaticRestriction interface/role.

--- lib/Pear/LocalLoop/Algorithm/IStaticRestriction.pm
+++ lib/Pear/LocalLoop/Algorithm/IStaticRestriction.pm
@@ -2,6 +2,9 @@ package Pear::LocalLoop::Algorithm::IStaticRestriction;
 
 use Moo::Role;
 
+#This removes transactions from the "ProcessedTransactions" table before processing begins.
+#They are removed as the context which these restrictions are applied in is static (no-context)
+#hence in all calculations they will be discounted, so they may as well be removed.
 sub applyStaticRestriction
 {
   die "applyStaticRestriction has not been implemented.";
