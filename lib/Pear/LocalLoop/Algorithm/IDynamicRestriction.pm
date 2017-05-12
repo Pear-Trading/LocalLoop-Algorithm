6a1ffc07d3ab52e1917f2301f738d846b9c5b74c
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-12 04:09:53 +0100

    Added comments to the IDynamicRestriction role/interface.

--- lib/Pear/LocalLoop/Algorithm/IDynamicRestriction.pm
+++ lib/Pear/LocalLoop/Algorithm/IDynamicRestriction.pm
@@ -2,6 +2,11 @@ package Pear::LocalLoop::Algorithm::IDynamicRestriction;
 
 use Moo::Role;
 
+#Similar to "IStaticRestriction" but the restriction is dependent on the transaction
+#context it's working with.
+#1st param $self.
+#2nd param whether this is the first dynamic restriction called, hence must overwrite 
+#any previous state in the "Included" column of the "ProcessedTransactions" table.
 sub applyDynamicRestriction
 {
   die "applyDynamicRestriction has not been implemented.";
