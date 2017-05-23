ae44a02d9f8ff4b32b4017e1e1f52656951edd43
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-23 07:04:28 +0100

    Update init script with new modules.

--- init.pl
+++ init.pl
@@ -14,6 +14,8 @@ use Pear::LocalLoop::Algorithm::ProcessingTypeContainer;
 use Pear::LocalLoop::Algorithm::StaticRestriction::RemoveTransactionsThatCannotFormALoop;
 use Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyTransactionsWhichFromUserMatchesOurToUser;
 use Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyAfterCurrentTransaction;
+use Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyTransactionsNotExtendedOntoYet;
+use Pear::LocalLoop::Algorithm::Heuristic::None;
 use Pear::LocalLoop::Algorithm::TransactionOrder::EarliestFirst;
 use Pear::LocalLoop::Algorithm::TransactionOrder::LargestTransactionValueFirst;
 
@@ -36,13 +38,16 @@ my $staticRestrictions = [$rst];
 
 my $matchId = Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyTransactionsWhichFromUserMatchesOurToUser->new();
 my $afterCurrent = Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyAfterCurrentTransaction->new();
-my $dynamicRestrictions = [$matchId, $afterCurrent];
+my $extendedOnto = Pear::LocalLoop::Algorithm::DynamicRestriction::AllowOnlyTransactionsNotExtendedOntoYet->new();
+my $dynamicRestrictions = [$matchId, $extendedOnto, $afterCurrent];
 
-my $heuristics = [];
+my $none = Pear::LocalLoop::Algorithm::Heuristic::None->new();
+my $heuristics = [$none];
 
 my $hash = {
   staticRestrictionsArray => $staticRestrictions,
   dynamicRestrictionsArray => $dynamicRestrictions,
+  heuristicArray => $heuristics,
   transactionOrder => Pear::LocalLoop::Algorithm::TransactionOrder::EarliestFirst->new(),
 };
 
