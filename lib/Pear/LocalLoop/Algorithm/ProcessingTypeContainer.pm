588f612a1b5b6ca4117e69e6b2b31ad6f12cd8de
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-23 17:09:39 +0100

    Added debug statements to the ProcessingTypeContainer to help validate that it's working as expected.

--- lib/Pear/LocalLoop/Algorithm/ProcessingTypeContainer.pm
+++ lib/Pear/LocalLoop/Algorithm/ProcessingTypeContainer.pm
@@ -2,6 +2,7 @@ package Pear::LocalLoop::Algorithm::ProcessingTypeContainer;
 
 use Moo;
 use Pear::LocalLoop::Algorithm::Debug;
+use DBI;
 
 has staticRestrictionsArray => (
   is => 'ro',
@@ -92,6 +93,8 @@ sub applyDynamicRestrictionsAndHeuristics {
   debugMethodStart(__PACKAGE__, "applyDynamicRestrictionsAndHeuristics", __LINE__);
   my ($self, $transactionId, $chainId) = @_;
   
+  debugMethodMiddle(__LINE__, "InputParams: TransactionId:$transactionId ChainId:$chainId");
+  
   if (! defined $transactionId) {
     die "transactionId cannot be undefined."
   }
@@ -102,11 +105,13 @@ sub applyDynamicRestrictionsAndHeuristics {
   my $isFirst = 1;
   foreach my $dynamicRestriction (@{$self->dynamicRestrictionsArray()}) {
     $dynamicRestriction->applyDynamicRestriction($transactionId, $chainId, $isFirst);
+    $self->_dumpTransactionsIncluded();
     $isFirst = 0;
   }
   
   foreach my $heuristic (@{$self->heuristicArray()}) {
     $heuristic->applyHeuristic($transactionId, $chainId, $isFirst);
+    $self->_dumpTransactionsIncluded();
     $isFirst = 0;
   }    
   debugMethodEnd(__PACKAGE__, "applyDynamicRestrictionsAndHeuristics", __LINE__);
@@ -119,10 +124,53 @@ sub applyHeuristicsCandinates {
   my $isFirst = 1;
   foreach my $heuristic (@{$self->heuristicArray()}) {
     $heuristic->applyHeuristicCandinates($isFirst);
+    $self->_dumpCandinateTransactionsIncluded();
     $isFirst = 0;
   }    
   
   debugMethodEnd(__PACKAGE__, "applyHeuristicsCandinates", __LINE__);
+}
+
+sub _dumpTransactionsIncluded {
+  #debugMethodStart(__PACKAGE__, "_dumpTransactionsIncluded", __LINE__);
+  my ($self) = @_;
+  my $dbh = Pear::LocalLoop::Algorithm::Main->dbi();
+  
+  my $statement = $dbh->prepare("SELECT TransactionId, Included FROM ProcessedTransactions ORDER BY TransactionId");
+  $statement->execute();
+  
+  my $string = "";
+  my $isFirst = 1;
+  while (my ($transactionId, $included) = $statement->fetchrow_array) {
+    my $comma = ($isFirst ? "" : ", ");
+    $string = $string . $comma . "$transactionId=" . ($included ? "T" : "F");
+    $isFirst = 0;
+  }
+  
+  debugMethodMiddle(__LINE__, "TransactionsIncludedDump: ".$string);
+  
+  #debugMethodEnd(__PACKAGE__, "_dumpTransactionsIncluded", __LINE__);
+}
+
+sub _dumpCandinateTransactionsIncluded {
+  #debugMethodStart(__PACKAGE__, "_dumpCandinateTransactionsIncluded", __LINE__);
+  my ($self) = @_;
+  my $dbh = Pear::LocalLoop::Algorithm::Main->dbi();
+  
+  my $statement = $dbh->prepare("SELECT CandinateTransactionsId, Included FROM CandinateTransactions ORDER BY CandinateTransactionsId");
+  $statement->execute();
+  
+  my $string = "";
+  my $isFirst = 1;
+  while (my ($transactionId, $included) = $statement->fetchrow_array) {
+    my $comma = ($isFirst ? "" : ", ");
+    $string = $string . $comma . "$transactionId=" . ($included ? "T" : "F");
+    $isFirst = 0;
+  }
+  
+  debugMethodMiddle(__LINE__, "CandinateTransactionsIncludedDump: ".$string);
+  
+  #debugMethodEnd(__PACKAGE__, "_dumpCandinateTransactionsIncluded", __LINE__);
 }
 
 
