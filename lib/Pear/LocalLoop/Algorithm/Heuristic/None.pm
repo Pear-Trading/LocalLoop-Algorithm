8817638613f4353297eb1591611dad7f52d60034
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-28 06:36:19 +0100

    Refactor None to use prepared statements per instance rather than per function call.

--- lib/Pear/LocalLoop/Algorithm/Heuristic/None.pm
+++ lib/Pear/LocalLoop/Algorithm/Heuristic/None.pm
@@ -7,12 +7,56 @@ use Pear::LocalLoop::Algorithm::Debug;
 extends 'Pear::LocalLoop::Algorithm::Role::AbstractDatabaseModifier';
 with ('Pear::LocalLoop::Algorithm::Role::IHeuristic');
 
+has selectChainMinimumTransactionId => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT MIN(TransactionId) FROM ProcessedTransactions_ViewIncluded WHERE ? < TransactionId");
+  },
+  lazy => 1,
+);
+
+has selectChainMinimumTransactionIdFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT MIN(TransactionId) FROM ProcessedTransactions WHERE ? < TransactionId");
+  },
+  lazy => 1,
+);
+
+has selectChainNone => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE ProcessedTransactions SET Included = 0 WHERE Included != 0 AND TransactionId != ?");
+  },
+  lazy => 1,
+);
+
+has selectChainNoneFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE ProcessedTransactions SET Included = 1 WHERE Included = 0 AND TransactionId = ?");
+  },
+  lazy => 1,
+);
+
+has selectChainNoSelectedTransaction => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE ProcessedTransactions SET Included = 0 WHERE Included != 0");
+  },
+  lazy => 1,
+);
+
 
 sub applyHeuristic {
   debugMethodStart();
   
   my ($self, $transactionId, $chainId, $isFirst) = @_;
-  my $dbh = $self->dbh();
   
   #Chain id is not used so it does not matter if it's undefined.
   if ( ! defined $transactionId) {
@@ -21,34 +65,66 @@ sub applyHeuristic {
   elsif ( ! defined $isFirst) {
     die "isFirstRestriction cannot be undefined.";
   }
-
-  my $nextTransactionId = undef;
   
-  if ($isFirst) {
-    $nextTransactionId = @{$dbh->selectrow_arrayref("SELECT MIN(TransactionId) FROM ProcessedTransactions WHERE ? < TransactionId", undef, ($transactionId))}[0];
-  }
-  else {
-    $nextTransactionId = @{$dbh->selectrow_arrayref("SELECT MIN(TransactionId) FROM ProcessedTransactions_ViewIncluded WHERE ? < TransactionId", undef, ($transactionId))}[0];    
-  }
+  my $statementMinTransaction = ($isFirst ? $self->selectChainMinimumTransactionIdFirst() : $self->selectChainMinimumTransactionId());
+  $statementMinTransaction->execute($transactionId);
   
+  my ($nextTransactionId) = $statementMinTransaction->fetchrow_array();
+ 
   #say "next: $nextTransactionId";
   
   #There is at least one next transaction id.
   if (defined $nextTransactionId)
   {
-    $dbh->prepare("UPDATE ProcessedTransactions SET Included = 0 WHERE Included != 0 AND TransactionId != ?")->execute($nextTransactionId);
+    $self->selectChainNone()->execute($nextTransactionId);
     
     if ($isFirst) {
-      $dbh->prepare("UPDATE ProcessedTransactions SET Included = 1 WHERE Included = 0 AND TransactionId = ?")->execute($nextTransactionId);
+      $self->selectChainNoneFirst()->execute($nextTransactionId);
     }
   }
   #No next value so set all valued to not be included.
   else {
-    $dbh->prepare("UPDATE ProcessedTransactions SET Included = 0 WHERE Included != 0")->execute();
+    $self->selectChainNoSelectedTransaction()->execute();
   }
   
   debugMethodEnd();
 };
+
+has statementCandinatesMinimumTransactionId => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT CandinateTransactionsId, MIN(TransactionTo_FK) FROM CandinateTransactions_ViewIncluded");
+  },
+  lazy => 1,
+);
+
+has statementCandinatesMinimumTransactionIdFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT CandinateTransactionsId, MIN(TransactionTo_FK) FROM CandinateTransactions");
+  },
+  lazy => 1,
+);
+
+has statementCandinatesNone => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE CandinateTransactions SET Included = 0 WHERE Included != 0 AND CandinateTransactionsId != ?");
+  },
+  lazy => 1,
+);
+
+has statementCandinatesNoneFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE CandinateTransactions SET Included = 1 WHERE Included = 0 AND CandinateTransactionsId = ?");
+  },
+  lazy => 1,
+);
 
 sub applyHeuristicCandinates {
   debugMethodStart();
@@ -56,31 +132,60 @@ sub applyHeuristicCandinates {
   my ($self, $isFirst) = @_;
   my $dbh = $self->dbh();
 
-  my $nextTransactionId = undef;
-  my $candinateTransactionsId = undef;
-  
-  if ($isFirst) {
-    ($candinateTransactionsId, $nextTransactionId) = $dbh->selectrow_array("SELECT CandinateTransactionsId, MIN(TransactionTo_FK) FROM CandinateTransactions", undef, ());
-  }
-  else {
-    ($candinateTransactionsId, $nextTransactionId) = $dbh->selectrow_array("SELECT CandinateTransactionsId, MIN(TransactionTo_FK) FROM CandinateTransactions_ViewIncluded", undef, ());    
-  }
-  
-  
-  #If this is undef all are not included anyway.
-  
+  my $statementMinTransaction = ($isFirst ? $self->statementCandinatesMinimumTransactionIdFirst() : $self->statementCandinatesMinimumTransactionId());
+  $statementMinTransaction->execute();
+  my ($candinateTransactionsId, $nextTransactionId) = $statementMinTransaction->fetchrow_array();
+
+  #If this is undef all are not included anyway.  
   #There is at least one next transaction id. We only need the candinate transaction id as that identifies one row.
   if (defined $candinateTransactionsId)
   {
-    $dbh->prepare("UPDATE CandinateTransactions SET Included = 0 WHERE Included != 0 AND CandinateTransactionsId != ?")->execute($candinateTransactionsId);
+    $self->statementCandinatesNone()->execute($candinateTransactionsId);
     
     if ($isFirst) {
-      $dbh->prepare("UPDATE CandinateTransactions SET Included = 1 WHERE Included = 0 AND CandinateTransactionsId = ?")->execute($candinateTransactionsId);
+      $self->statementCandinatesNoneFirst()->execute($candinateTransactionsId);
     }
   }
   
   debugMethodEnd();
 };
+
+
+has statementLoopsMinimumTransactionId => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT LoopId FROM LoopInfo_ViewIncluded ORDER BY FirstTransactionId_FK ASC, LastTransactionId_FK ASC");
+  },
+  lazy => 1,
+);
+
+has statementLoopsMinimumTransactionIdFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("SELECT LoopId FROM LoopInfo ORDER BY FirstTransactionId_FK ASC, LastTransactionId_FK ASC");
+  },
+  lazy => 1,
+);
+
+has statementLoopsNone => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE LoopInfo SET Included = 0 WHERE Included != 0 AND LoopId != ?");
+  },
+  lazy => 1,
+);
+
+has statementLoopsNoneFirst => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->dbh()->prepare("UPDATE LoopInfo SET Included = 1 WHERE Included = 0 AND LoopId = ?");
+  },
+  lazy => 1,
+);
 
 sub applyHeuristicLoops {
   debugMethodStart();
@@ -88,23 +193,15 @@ sub applyHeuristicLoops {
   my ($self, $isFirst) = @_;
   my $dbh = $self->dbh();
 
-  my $earliestLoop = undef;
-  if ($isFirst) {
-    my $statementEarliestLoop = $dbh->prepare("SELECT LoopId FROM LoopInfo ORDER BY FirstTransactionId_FK ASC, LastTransactionId_FK ASC");
-    $statementEarliestLoop->execute();
-    ($earliestLoop) = $statementEarliestLoop->fetchrow_array();
-  }
-  else {
-    my $statementEarliestLoop = $dbh->prepare("SELECT LoopId FROM LoopInfo_ViewIncluded ORDER BY FirstTransactionId_FK ASC, LastTransactionId_FK ASC");
-    $statementEarliestLoop->execute();
-    ($earliestLoop) = $statementEarliestLoop->fetchrow_array();
-  }
+  my $statementEarliestLoop = ($isFirst ? $self->statementLoopsMinimumTransactionIdFirst() : $self->statementLoopsMinimumTransactionId());
+  $statementEarliestLoop->execute();
+  my ($earliestLoop) = $statementEarliestLoop->fetchrow_array();
   
   if (defined $earliestLoop) {
-    $dbh->prepare("UPDATE LoopInfo SET Included = 0 WHERE Included != 0 AND LoopId != ?")->execute($earliestLoop);
+    $self->statementLoopsNone()->execute($earliestLoop);
   
     if ($isFirst) {
-      $dbh->prepare("UPDATE LoopInfo SET Included = 1 WHERE Included = 0 AND LoopId = ?")->execute($earliestLoop);
+      $self->statementLoopsNoneFirst()->execute($earliestLoop);
     }
   }
 
