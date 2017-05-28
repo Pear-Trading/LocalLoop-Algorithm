efe115c0c270306fe5fddab68b0384b4e5e8d392
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-28 05:38:31 +0100

    Refactor RemoveTransactionsThatCannotFormALoop to use prepared statements per instance rather than per function call.

--- lib/Pear/LocalLoop/Algorithm/StaticRestriction/RemoveTransactionsThatCannotFormALoop.pm
+++ lib/Pear/LocalLoop/Algorithm/StaticRestriction/RemoveTransactionsThatCannotFormALoop.pm
@@ -7,31 +7,83 @@ use Pear::LocalLoop::Algorithm::Debug;
 extends 'Pear::LocalLoop::Algorithm::Role::AbstractDatabaseModifier';
 with ('Pear::LocalLoop::Algorithm::Role::IStaticRestriction');
 
+has tableName => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    return $self->uniqueTableName(ref($self), "0");
+  },
+  lazy => 1, 
+);
+
+has statementDropTable => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    my $tableNameZero = $self->tableName();
+    return $self->dbh()->prepare("DROP TABLE IF EXISTS $tableNameZero");
+  },
+  lazy => 1, 
+);
+
+has statementCreateTable => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    my $tableNameZero = $self->tableName();
+    return $self->dbh()->prepare("CREATE TABLE $tableNameZero (ZeroIds INTEGER PRIMARY KEY)");
+  },
+  lazy => 1, 
+);
+
+has statementInsertUserIdsOnlyInToUserIdAttribute => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    my $tableNameZero = $self->tableName();
+    return $self->dbh()->prepare("INSERT OR IGNORE INTO $tableNameZero (ZeroIds) SELECT ProcessedTransactions.ToUserId FROM ProcessedTransactions EXCEPT SELECT ProcessedTransactions.FromUserId FROM ProcessedTransactions");
+  },
+  lazy => 1, 
+);
+
+has statementInsertUserIdsOnlyInFromUserIdAttribute => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    my $tableNameZero = $self->tableName();
+    return $self->dbh()->prepare("INSERT OR IGNORE INTO $tableNameZero (ZeroIds) SELECT ProcessedTransactions.FromUserId FROM ProcessedTransactions EXCEPT SELECT ProcessedTransactions.ToUserId FROM ProcessedTransactions");
+  },
+  lazy => 1, 
+);
+
+has statementDeleteTransactionsThatCantFormLoops => (
+  is => 'ro',
+  default => sub {
+    my ($self) = @_;
+    my $tableNameZero = $self->tableName();
+    return $self->dbh()->prepare("DELETE FROM ProcessedTransactions WHERE FromUserId IN (SELECT ZeroIds FROM $tableNameZero) OR ToUserId IN (SELECT ZeroIds FROM $tableNameZero)");
+  },
+  lazy => 1, 
+);
 
 sub applyStaticRestriction{
   debugMethodStart();
   my ($self) = @_;
-  my $dbh = $self->dbh;
-
-  my $tableNameZero = $self->uniqueTableName(__PACKAGE__,"0") ;  
   
-  $dbh->do("DROP TABLE IF EXISTS $tableNameZero");
-  $dbh->do("CREATE TABLE $tableNameZero (ZeroIds INTEGER PRIMARY KEY)");
+  $self->statementDropTable()->execute();
+  $self->statementCreateTable()->execute();
 
   my $loop = 1;
   while ($loop != 0) {
     $loop = 0;
- 
-    #$dbh->do("DELETE FROM $tableNameZero");
     
-    $dbh->do("INSERT OR IGNORE INTO $tableNameZero (ZeroIds) SELECT ProcessedTransactions.ToUserId FROM ProcessedTransactions EXCEPT SELECT ProcessedTransactions.FromUserId FROM ProcessedTransactions");
-    $dbh->do("INSERT OR IGNORE INTO $tableNameZero (ZeroIds) SELECT ProcessedTransactions.FromUserId FROM ProcessedTransactions EXCEPT SELECT ProcessedTransactions.ToUserId FROM ProcessedTransactions");  
+    $self->statementInsertUserIdsOnlyInToUserIdAttribute()->execute();
+    $self->statementInsertUserIdsOnlyInFromUserIdAttribute()->execute();
     
-    my $del1 = $dbh->do("DELETE FROM ProcessedTransactions WHERE FromUserId IN (SELECT ZeroIds FROM $tableNameZero)"); 
-    my $del2 = $dbh->do("DELETE FROM ProcessedTransactions WHERE ToUserId IN (SELECT ZeroIds FROM $tableNameZero)"); 
+    my $del1 = $self->statementDeleteTransactionsThatCantFormLoops()->execute(); 
     
     #If there was more than 1 deleted keep on deleting.
-    if (0 < ($del1 + $del2)) {
+    if (0 < $del1) {
       $loop = 1;
     }
     #print "looped";
