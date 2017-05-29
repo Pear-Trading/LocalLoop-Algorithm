6460a71d8ef4b145c8a89189cb8bbfc2ccf7da08
Paul Dill <p.dill@shadowcat.co.uk>
2017-05-29 03:12:45 +0100

    Update documentation on IDynamicRestriction.

--- lib/Pear/LocalLoop/Algorithm/Role/IDynamicRestriction.pm
+++ lib/Pear/LocalLoop/Algorithm/Role/IDynamicRestriction.pm
@@ -13,9 +13,9 @@ sub initAfterStaticRestrictions {
 #Similar to "IStaticRestriction" but the restriction is dependent on the transaction
 #context it's working with.
 #1st param $self.
-#2nd param current transaction id.
-#3rd param whether this is the first dynamic restriction called, hence 
+#2rd param whether this is the first dynamic restriction called, hence 
 #any previous state in the "Included" column of the "ProcessedTransactions" table.
+#3rd param ChainGenerationContext instance.
 sub applyDynamicRestriction {
   die "applyDynamicRestriction has not been implemented.";
 };
