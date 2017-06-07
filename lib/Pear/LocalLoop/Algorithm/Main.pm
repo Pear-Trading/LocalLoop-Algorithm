e3926e7472950490c23ebb51218aaca82185d21e
Paul Dill <p.dill@shadowcat.co.uk>
2017-06-07 23:18:30 +0100

    Refactor and comment Main.

--- lib/Pear/LocalLoop/Algorithm/Main.pm
+++ lib/Pear/LocalLoop/Algorithm/Main.pm
@@ -3,25 +3,27 @@ package Pear::LocalLoop::Algorithm::Main;
 use Moo;
 with 'MooX::Singleton';
 
-use Data::Dumper;
-use DBI;
-use Pear::LocalLoop::Algorithm::StaticRestriction::RemoveTransactionsThatCannotFormALoop;
 use v5.10;
+use DBI;
+use Data::Dumper;
+use Carp::Always;
+use Pear::LocalLoop::Algorithm::StaticRestriction::RemoveTransactionsThatCannotFormALoop;
 use Pear::LocalLoop::Algorithm::Debug;
 use Pear::LocalLoop::Algorithm::ExtendedTransaction;
 use Pear::LocalLoop::Algorithm::ChainTransaction;
 use Pear::LocalLoop::Algorithm::ChainGenerationContext;
 use Pear::LocalLoop::Algorithm::LoopGenerationContext;
-use Carp::Always;
 
 
 #FIXME move into a config file and dynamically read it in.
+#Non-testing debug database.
 my $dbConfig = {
   dsn => "dbi:SQLite:dbname=transactions.db",
   user => undef,
   pass => undef,
 };
 
+#Testing debug database.
 my $dbTestConfig = {
 #  dsn => "dbi:SQLite:dbname=transactions-test.db",
   dsn => "dbi:SQLite:dbname=:memory:",
@@ -29,24 +31,28 @@ my $dbTestConfig = {
   pass => undef,
 };
 
-
-#  return DBI->connect($dbConfig->{dsn},$dbConfig->{user},$dbConfig->{pass}) or die "Could not connect";
+#Create new non-testing database interface handle.
 sub _dbi {
   my $self = shift;
-  return DBI->connect($dbConfig->{dsn},$dbConfig->{user},$dbConfig->{pass});
+  my $handle = DBI->connect($dbConfig->{dsn},$dbConfig->{user},$dbConfig->{pass}) or die "Could not connect";
+  return $handle;
 };
 
+#Create new Testing database interface handle.
+#Note: with in memory databases this will generate a new database each time.
 sub _dbi_test {
   my $self = shift;
   return DBI->connect($dbTestConfig->{dsn},$dbTestConfig->{user},$dbTestConfig->{pass});
 };
 
+#Main's handle to the database
 has dbh => (
   is => 'ro',
   default => sub { return dbi(); },
   lazy => 1,
 );
 
+#Static functions to set or clear testing mode.
 sub setTestingMode {
   $ENV{'MODE'} = "testing";
 }
@@ -55,6 +61,7 @@ sub clearTestingMode {
   $ENV{'MODE'} = undef;
 }
 
+#Get database handle taking into consideration whether we are in testing mode or not.
 sub dbi {
   my $mode = $ENV{'MODE'};
   if (defined $mode && $mode eq "testing") {
@@ -64,6 +71,8 @@ sub dbi {
     return _dbi();
   }
 }
+
+
 
 has _statementDeleteTuplesProcessedTransactions => (
   is => 'ro', 
@@ -97,35 +106,42 @@ sub process {
   debugMethodMiddle("Inputted settings:");
   say ("\n".Dumper($settings));
     
-    
+  #Disable auto commit to increase processing speed.
   $dbh->{AutoCommit} = 0;
   
+  #Clear the previous processing transactions state and clone the original transactions.
   $self->_statementDeleteTuplesProcessedTransactions()->execute();
   $self->_statementIntoTuplesFromOriginalTransactionsIntoProcessedTransactions()->execute();
   
-  $settings->init();
+  $settings->init(); #Initialise the settings
     
   $settings->applyStaticRestrictions();
-  $settings->initAfterStaticRestrictions();
+  $settings->initAfterStaticRestrictions(); #Initialise post static restrictions.
   
   $dbh->commit();
   
   debugMethodMiddle("Before loop");
   
   my $counter = 1;
+  
+  #Select a starting point for loop generation.
   for (my $nextTransactionId = $settings->nextTransactionId(); 
     defined $nextTransactionId; 
     $nextTransactionId = $settings->nextTransactionId(), $counter++)
   {
-    say "$counter";
+    say "$counter"; #Keep track of how well it's doing.
+    
     debugMethodMiddle("TransactionLoop: $nextTransactionId");
+    
     $self->_loopGeneration($settings, $nextTransactionId);
     
+    #Incremented commit so state can be inspected, also if it crashes then loops will be safe.
     if (($counter % 250) == 0) {
       $dbh->commit();
     }
-
   }
+  
+  debugMethodMiddle("After loop");
   
   #This may be here or the last line in the for loop, depending on when you want the loops to be selected.
   $self->_loopSelection($settings);
@@ -163,6 +179,7 @@ has _statementUpdateLoopInfoSetActiveToZeroWhereActiveIsNotZero => (
   lazy => 1,
 );
 
+#Select loops given the settings.
 sub _loopSelection {
   debugMethodStart();
   my ($self, $settings) = @_;
@@ -178,13 +195,17 @@ sub _loopSelection {
   #Insert one loop at a time as the activating of a loop may break the other loops (consistency of the loops).
   my $loopId = undef; 
   do {
-    say "$counter";
+    say "$counter"; #Keeps track of progress
+    
     $settings->applyLoopDynamicRestrictionsAndHeuristics();
     $statementSelectOneIncludedLoopId->execute();
     ($loopId) = $statementSelectOneIncludedLoopId->fetchrow_array();
+    
+    #Set a loop id to be active while it can fit in.
     if (defined $loopId) {
       $statementSetLoopToActive->execute($loopId);
     }
+    
     $counter++;
   } while (defined $loopId); #Continue looping while loops can co-exist.
   
@@ -302,22 +323,21 @@ has _statementInsertIntoLoopsSpecifiedLoopIdWithAllOfTheTransactionsInAChain => 
   lazy => 1,
 );
 
-
+#Generates loops based on the settings and transaction id selected.
 sub _loopGeneration {
   debugMethodStart();
   #we assume the nextTransaction id is valid.
   my ($self, $settings, $nextTransactionId) = @_;
-  my $dbh = $self->dbh;
   
   debugMethodMiddle("NextTransactionId:$nextTransactionId");
 
+  #Clear previous loop generation state.
   $self->_statementDeleteTuplesCandidateTransactions()->execute();  
   $self->_statementDeleteTuplesBranchedTransactions()->execute();
   $self->_statementDeleteTuplesChains()->execute();
   $self->_statementDeleteTuplesChainInfo()->execute();
   
   my $statementTransactionValue = $self->_statementSelectFromUserIdAndValueOfATransaction();
-
   $statementTransactionValue->execute($nextTransactionId); 
   my ($fromUserId, $transactionValue) = $statementTransactionValue->fetchrow_array();
   
@@ -325,24 +345,24 @@ sub _loopGeneration {
     userIdWhichCreatesALoop => $fromUserId,
   });
   
-  debugMethodMiddle(" Candidate fromUserId:$fromUserId value:$transactionValue");
+  debugMethodMiddle("Candidate fromUserId:$fromUserId value:$transactionValue");
   
   my $candidateId = $self->_newCandidateTransactionsId();
   my $statementInsertCandidate = $self->_statementInsertIntoCandidateTransactionsAllParamsExceptIncludedAndHeuristic();
- 
 
   #Insert initial transaction.
   $statementInsertCandidate->execute($candidateId, undef, undef, $nextTransactionId, $transactionValue, 1, $transactionValue, 1);
-  debugMethodMiddle("InsertedInitalCandidate");
+  debugMethodMiddle("Inserted inital transaction candidate");
   
   my $extendedTransaction = undef;
   
+  #Loop while it has not found a loop and has candidate transactions left then continue looping
   while ( ! ($extendedTransaction = $self->_candidateSelection($settings, $loopGenerationContext))->hasFinished($loopGenerationContext) ) {
-    debugMethodMiddle("1st while loop start");
+    debugMethodMiddle("Loop generation while loop start");
+    
     $self->_candidateInsertion($settings, $extendedTransaction, $loopGenerationContext);
     
-    #sleep (10);
-    debugMethodMiddle("1st while loop end");
+    debugMethodMiddle("Loop generation while loop end");
   }
   
   #Insert all of the remaining candidate transactions, so equal candidates can be found.
@@ -354,8 +374,8 @@ sub _loopGeneration {
   }
   
 
+  #What chains are loops?
   my $statementLoops = $self->_statementSelectTheChainIdWhichCouldFormALoop();
-    
   $statementLoops->execute($fromUserId);
   
   #Store a list as we can't have a select and insert statement executed at the same time on a SQLite database.
@@ -366,31 +386,33 @@ sub _loopGeneration {
   
   #say Dumper($chainIdsWhichAreLoops);
   
+  
   my $statementGetChainMinMax = $self->_statementSelectFirstAndLastTransactionsInASpecfiedChain();
   my $statementGetChainInfoId = $self->_statementSelectChainInfoIdFromChainsGivenSpecifiedChainIdAndTransactionId();
   my $statementGetChainStats = $self->_statementSelectChainStatsGivenAChainInfoId();
-
-  
   my $statementInsertStats = $self->_statementInsertIntoLoopInfoNewTupleExcludingActiveIncludedAndHeuristic();
   my $statementInsert = $self->_statementInsertIntoLoopsSpecifiedLoopIdWithAllOfTheTransactionsInAChain();
   
-  
+  #Generate a list of new loop ids.
   my $newLoopIds = [];
   #TODO think about how to detect duplicate loops.
   foreach my $chainId (@{$chainIdsWhichAreLoops}) {
     my $newUniqueLoopId = $self->_newLoopId();
     push(@$newLoopIds, $newUniqueLoopId);
     
+    #Get first and last transaction.
     $statementGetChainMinMax->execute($chainId);
     my ($minTransactionId, $maxTransactionId) = $statementGetChainMinMax->fetchrow_array();
     
+    #Get information about the chain that will be a loop.
     $statementGetChainInfoId->execute($chainId, $maxTransactionId);
     my ($chainStatsId) = $statementGetChainInfoId->fetchrow_array();
-    
     $statementGetChainStats->execute($chainStatsId);
     my ($minimumValue, $length, $totalValue, $numberOfMinimumValues) = $statementGetChainStats->fetchrow_array();
     
+    #Insert the chain information into the loops tables.
     $statementInsertStats->execute($newUniqueLoopId, $minTransactionId, $maxTransactionId, $minimumValue, $length, $totalValue, $numberOfMinimumValues);
+    #Copy a loop from the chains table into the loops table.
     $statementInsert->execute($newUniqueLoopId, $chainId);
   }
   
@@ -427,6 +449,7 @@ has _statementSelectTheNumberOfTransactionsFromTheSamePointInTheChain => (
   lazy => 1,
 );
 
+#Given a selected candidate transaction insert the next candidate transactions using the from transaction and to transaction.
 #Note: the test for this function is limited to that only the differences in the pass with 2 transactions (from and to)
 #are tested. We assume if it can execute the same code path with just the one transaction (to) it will work
 #when there is 2.
@@ -455,8 +478,10 @@ sub _candidateInsertion {
     push(@$transactionsToAnalyse, $fromTransaction);
   }
   
+  #For a specified transaction insert the next candidate transactions.
   foreach my $chainTransaction (@{$transactionsToAnalyse}) {
     debugBraceStart("ForEachLoop 2Trans.");
+    
     my $chainId = $chainTransaction->chainId();
     my $transactionId = $chainTransaction->transactionId();
     my $fromTo = $chainTransaction->fromTo();
@@ -485,15 +510,15 @@ sub _candidateInsertion {
       currentTransactionId => $transactionId, 
       userIdWhichCreatesALoop => $userIdThatFormsALoop,
     });
-    
+
     $settings->applyChainDynamicRestrictionsAndHeuristics($chainGenerationContext);
 
     debugMethodMiddle("WhileLoop: ForEachLoop. Done heuristics");
     
+    #Get the information about a transaction uptil its point in the chain
     my $statementChainInfoId = $self->_statementSelectChainInfoIdFromChainsGivenSpecifiedChainIdAndTransactionId();
     $statementChainInfoId->execute($chainId, $transactionId);
     my ($chainStatsId) = $statementChainInfoId->fetchrow_array();
-    
     my $statementChainStats = $self->_statementSelectChainStatsGivenAChainInfoId();
     $statementChainStats->execute($chainStatsId);    
     my ($minimumValue, $length, $totalValue, $numberOfMinimumValues) = $statementChainStats->fetchrow_array();
@@ -513,16 +538,17 @@ sub _candidateInsertion {
       my $isValueLower = ($candidateValue < $minimumValue);
       my $isValueSame = ($candidateValue == $minimumValue);
       
+      #Calculate stats of the candidate transaction
       my $thisMinimumValue = ($isValueLower ? $candidateValue : $minimumValue);
       my $thisLength = $length + 1;
       my $thisTotalValue = $totalValue + $candidateValue;
       my $thisNumberOfMinimumValues = ($isValueLower ? 1 : ($isValueSame ? ($numberOfMinimumValues + 1) : $numberOfMinimumValues));
-      
 
       my $candidateId = $self->_newCandidateTransactionsId();
       
       debugMethodMiddle("WhileLoop: ForEachLoop. EnteredCandidateValues CandidateId:$candidateId chainId:$chainId fromTransaction:$transactionId toTransaction:$candidateTransactionId MinValue:$thisMinimumValue Length:$thisLength TotalValue:$thisTotalValue NumOfMinValues:$thisNumberOfMinimumValues");
       
+      #Insert the new candidate transactions
       $statementInsertCandidate->execute($candidateId, $chainId, $transactionId, $candidateTransactionId, $thisMinimumValue, $thisLength, $thisTotalValue, $thisNumberOfMinimumValues);
       
       debugBraceEnd("While loop best next trans.");      
@@ -607,7 +633,7 @@ has _statementSelectCandidateTransactionInformationWhenItsIncluded => (
   lazy => 1,
 );
 
-
+#Select which candinate transaction to extend from.
 sub _candidateSelection {
   debugMethodStart();  
   my ($self, $settings, $loopGenerationContextInstance) = @_;
@@ -616,6 +642,7 @@ sub _candidateSelection {
   my $statementInsertChainStats = $self->_statementInsertIntoChainInfoIdMinValueLengthTotalValueAndNumMinValues();
   my $statementDelete = $self->_statementDeleteCandidateTransactionBasedOnItsId();
   
+  #See if there are any first transactions will null from transaction and chain id.
   my $statementSelectNullFrom = $self->_statementSelectCandidateTransactionInformationWhenItsAFirstTransaction();
   $statementSelectNullFrom->execute();
   my $nullResult = $statementSelectNullFrom->fetchrow_arrayref();
@@ -626,9 +653,11 @@ sub _candidateSelection {
     debugMethodMiddle("Has first transaction");
     my ($candidateTransactionId, $transactionTo, $minimumValue, $length, $totalValue, $numberOfMinimumValues) = @{$nullResult};
     
+  #Delete the candidate from the database.
     my $numberRowsDeleted = $statementDelete->execute($candidateTransactionId);
     debugMethodMiddle("Deleted CandidateTransactionId:$candidateTransactionId");    
     
+    #Add the first transaction into the database with a new chain id.
     my $chainId = $self->_newChainId();
     my $newChainInfoId = $self->_newChainInfoId();  
     $statementInsertChainStats->execute($newChainInfoId, $minimumValue, $length, $totalValue, $numberOfMinimumValues);
@@ -651,20 +680,21 @@ sub _candidateSelection {
     debugMethodEnd();  
     return $transactionsToAnalyseNext;
   }
+  #There is no first transactions
   else {
     debugMethodMiddle("Hasn't first transaction");
   
     $settings->applyChainHeuristicsCandidates($loopGenerationContextInstance);
-    
+  
     my $statementSelectRows = $self->_statementSelectCandidateTransactionInformationWhenItsIncluded();
     $statementSelectRows->execute();
     
-    my $rowRef = $statementSelectRows->fetchrow_arrayref();
-    #say "Ref: " . Dumper ($rowRef);
+    my $candinateTransactionRowRef = $statementSelectRows->fetchrow_arrayref();
+    #say "Ref: " . Dumper ($candinateTransactionRowRef);
     
-    #hasn't got a row
-    if (! defined $rowRef) {
-      debugMethodMiddle("No row");
+    #There are no rows left
+    if (! defined $candinateTransactionRowRef) {
+      debugMethodMiddle("No candidate transaction rows left.");
       my $transactionsToAnalyseNext = Pear::LocalLoop::Algorithm::ExtendedTransaction->new({
         noCandidateTransactionsLeft => 1,
       });
@@ -673,30 +703,30 @@ sub _candidateSelection {
       debugMethodEnd();  
       return $transactionsToAnalyseNext;
     }
+    #There are candidates left.
     else {
-      debugMethodMiddle("Row");
+      debugMethodMiddle("Has a candidate transaction row.");
       my ($candidateTransactionId, $chainId, $transactionFrom, $transactionTo, $minimumValue, $length, $totalValue, $numberOfMinimumValues) =
-        @{$rowRef};
+        @{$candinateTransactionRowRef};
       debugMethodMiddle("Row values: chainId:$chainId transFrom:$transactionFrom transTo:$transactionTo miniValue:$minimumValue Length:$length TotalValue:$totalValue numMinVals:$numberOfMinimumValues");
-        
-      my $newChainInfoId = $self->_newChainInfoId();  
-    
+      
+      #Delete the candidate from the database.
       $statementDelete->execute($candidateTransactionId);
       debugMethodMiddle("Deleted CandidateTransactionId:$candidateTransactionId");
       
+      #Find the maximum transaction id to determine if the candidate is the last transaction or not.
       my $statementMaxTransactionIdForChain = $self->_statementSelectLastTransactionIdInAChain();
       $statementMaxTransactionIdForChain->execute($chainId);
-      
       my ($highestTransactionId) = $statementMaxTransactionIdForChain->fetchrow_array();
 
       #For this specific transaction the chain id must remain consistent thoughout the analysis, otherwise the application of
-      #DynamicRestriction::AllowOnlyTransactionsNotExtendedOntoYet will break;
+      #DynamicRestriction::AllowOnlyTransactionsNotExtendedOntoYet will break, So it's important it's before
+      #the branch management.
       my $fromTransaction = Pear::LocalLoop::Algorithm::ChainTransaction->new({
         transactionId => $transactionFrom,
         chainId => $chainId, 
         fromTo => 'from',
       });
-     
           
       #Adding of this transaction will create a branch in the chain.
       #Otherwise it will be equal as you can't have a the from transaction ever being higher because of MAX.
@@ -704,12 +734,15 @@ sub _candidateSelection {
         my $branchChainId = $self->_newChainId();
         debugMethodMiddle("SeparateBranches: PreviousBranchId:$chainId NewBranchId:$branchChainId");
         
+        #Add tuple noting it has branched
         my $statementInsertBranch = $self->_statementInserIntoBranchedTransactionChainIdFromTransactionIdAndToTransactionId();
         $statementInsertBranch->execute($chainId, $transactionFrom, $transactionTo);
         
+        #Select all transactions from a chain upto the point of branching
         my $statementChainsSelect = $self->_statementSelectTransactionIdAndChainInfoIdFromTheSpecifiedChainIdAndOnOrBeforeTheSpecifiedTrasactionId();
         $statementChainsSelect->execute($chainId, $transactionFrom);
         
+        #Clone the chain upto the branching point.
         while (my ($transactionId, $chainStatsId) = $statementChainsSelect->fetchrow_array()) {
           $statementInsertChains->execute($branchChainId, $transactionId, $chainStatsId);
         }
@@ -725,6 +758,8 @@ sub _candidateSelection {
         chainId => $chainId,
         fromTo => 'to',
       });
+      
+      my $newChainInfoId = $self->_newChainInfoId();  
       
       #Adds the non branched transaction or final branched transaction.
       $statementInsertChainStats->execute($newChainInfoId, $minimumValue, $length, $totalValue, $numberOfMinimumValues);
@@ -746,7 +781,6 @@ sub _candidateSelection {
 
 
 
-
 has _statementSelectMaxChainId => (
   is => 'ro', 
   default => sub {
@@ -756,6 +790,7 @@ has _statementSelectMaxChainId => (
   lazy => 1,
 );
 
+# Return a unique identifer that can be used as part of the primary key in the Chains table.
 sub _newChainId {
   debugMethodStart();
   
@@ -793,6 +828,7 @@ has _statementSelectMaxChainInfoId => (
   lazy => 1,
 );
 
+# Return a unique identifer that can be used as the primary key in the ChainInfo table.
 sub _newChainInfoId {
   debugMethodStart();
   
@@ -830,6 +866,7 @@ has _statementSelectMaxCandidateTransactionsId => (
   lazy => 1,
 );
 
+# Return a unique identifer that can be used as the primary key in the CandidateTransactions table.
 sub _newCandidateTransactionsId {
   debugMethodStart();
   
@@ -867,6 +904,8 @@ has _statementSelectMaxLoopId => (
   lazy => 1,
 );
 
+
+# Return a unique identifer that can be used as the primary key in the LoopId table.
 sub _newLoopId {
   debugMethodStart();
   
