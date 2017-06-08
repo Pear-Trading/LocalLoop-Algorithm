1e54f3f6454454c2e5d46f0d69138e61882c5a2b
Paul Dill <p.dill@shadowcat.co.uk>
2017-06-08 09:04:09 +0100

    Add API comment onto IStaticRestriction.

--- lib/Pear/LocalLoop/Algorithm/Role/IStaticRestriction.pm
+++ lib/Pear/LocalLoop/Algorithm/Role/IStaticRestriction.pm
@@ -6,6 +6,8 @@ use v5.10;
 #This removes transactions from the "ProcessedTransactions" table before processing begins.
 #They are removed as the context which these restrictions are applied in is static (no-context)
 #hence in all calculations they will be discounted, so they may as well be removed.
+
+#1st param - self.
 sub applyStaticRestriction {
   die "applyStaticRestriction has not been implemented.";
 };
