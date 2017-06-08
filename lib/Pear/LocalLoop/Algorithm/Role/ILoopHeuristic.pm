3f6cb17d98b83bc19e724e18ac2ae0f518e4a5fa
Paul Dill <p.dill@shadowcat.co.uk>
2017-06-08 09:05:12 +0100

    Add API comments onto ILoopHeuristic.

--- lib/Pear/LocalLoop/Algorithm/Role/ILoopHeuristic.pm
+++ lib/Pear/LocalLoop/Algorithm/Role/ILoopHeuristic.pm
@@ -12,6 +12,10 @@ sub initAfterStaticRestrictions {
   debugMethodEnd();
 }
 
+
+#1st param $self.
+#2nd param whether this is the first loop dynamic restriction/loop heuristic called, 
+#hence any previous state in the "Included" column of the "LoopInfo" table should be ignored.
 sub applyLoopHeuristic {
   die "applyLoopHeuristic has not been implemented.";
 };
