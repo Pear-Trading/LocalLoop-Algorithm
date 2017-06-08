6f4994937e2c296c546de29d0890654ae9332dec
Paul Dill <p.dill@shadowcat.co.uk>
2017-06-08 09:07:26 +0100

    Add API comments onto IChainHeuristic.

--- lib/Pear/LocalLoop/Algorithm/Role/IChainHeuristic.pm
+++ lib/Pear/LocalLoop/Algorithm/Role/IChainHeuristic.pm
@@ -10,10 +10,18 @@ sub initAfterStaticRestrictions {
   debugMethodEnd();
 }
 
+#1st param $self.
+#2rd param whether this is the first chain heuristic/chain dynamic restriction called, 
+#hence any previous state in the "Included" column of the "ProcessedTransactions" table should be ignored.
+#3rd param ChainGenerationContext instance.
 sub applyChainHeuristic {
   die "applyChainHeuristic has not been implemented.";
 };
 
+#1st param $self.
+#2rd param whether this is the first candidate transaction heuristic called, 
+#hence any previous state in the "Included" column of the "CandidateTransactions" table should be ignored.
+#3rd param LoopGenerationContext instance.
 sub applyCandidateTransactionHeuristic {
   die "applyCandidateTransactionHeuristic has not been implemented.";
 };
