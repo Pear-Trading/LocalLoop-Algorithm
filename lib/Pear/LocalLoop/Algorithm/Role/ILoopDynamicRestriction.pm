8bce14d1668c61d6b7d5b8f0d58270cd13ea2a8c
Paul Dill <p.dill@shadowcat.co.uk>
2017-06-08 09:06:05 +0100

    Refactor API comments of ILoopDynamicRestriction.

--- lib/Pear/LocalLoop/Algorithm/Role/ILoopDynamicRestriction.pm
+++ lib/Pear/LocalLoop/Algorithm/Role/ILoopDynamicRestriction.pm
@@ -10,11 +10,9 @@ sub initAfterStaticRestrictions {
   debugMethodEnd();
 }
 
-#Similar to "IStaticRestriction" but the restriction is dependent on the transaction
-#context it's working with.
 #1st param $self.
-#2nd param whether this is the first dynamic restriction called, hence 
-#any previous state in the "Included" column of the "LoopInfo" table.
+#2nd param whether this is the first loop dynamic restriction/loop heuristic called, 
+#hence any previous state in the "Included" column of the "LoopInfo" table should be ignored.
 sub applyLoopDynamicRestriction {
   die "applyLoopDynamicRestriction has not been implemented.";
 };
